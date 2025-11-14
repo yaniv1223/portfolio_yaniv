f6c34f6be8f4ed9e8c1dbf7dc742d082ba29be5a
yaniv1223 <157611742+yaniv1223@users.noreply.github.com>
2025-11-15 00:35:18 +0200

    update the github project
    update the github project

--- src/assets/sections/Projects/Projects.jsx
+++ src/assets/sections/Projects/Projects.jsx
@@ -1,25 +1,27 @@
-import Styles from './projectsStyles.module.css'
-import ProjectsCard from '../../common/ProjectCard'
-import viberr from '../../../assets/viberr.png'
-import freshBurger from '../../../assets/fresh-burger.png'
+import Styles from "./projectsStyles.module.css";
+import ProjectsCard from "../../common/ProjectCard";
+import BlitzVision from "../../../assets/blitz_vision.svg";
+import freshBurger from "../../../assets/fresh-burger.png";
 function Projects() {
-    return( 
-      <section id="projects" className={Styles.container}>
-        <h1 className="SectionTitle">Projects</h1>
-        <div className={Styles.projectsContainer}>
-         <ProjectsCard 
-         src={viberr} 
-         link="https://github.com/" 
-         h3="viberr"
-          p="Streaming App"/>
-           <ProjectsCard 
-         src={freshBurger} 
-         link="https://github.com/" 
-         h3="freshBurger"
-          p="Hamburger Restaurant"/>
-        </div>
-      </section>
-    )
+  return (
+    <section id="projects" className={Styles.container}>
+      <h1 className="SectionTitle">Projects</h1>
+      <div className={Styles.projectsContainer}>
+        <ProjectsCard
+          src={BlitzVision}
+          link="https://github.com/yaniv1223/Blitz_vision"
+          h3="BLITZ VISION"
+          p="AI Voice Assistant for the Visually Impaired"
+        />
+        <ProjectsCard
+          src={freshBurger}
+          link="https://github.com/"
+          h3="freshBurger"
+          p="Hamburger Restaurant"
+        />
+      </div>
+    </section>
+  );
 }
 
-export default Projects
+export default Projects;
